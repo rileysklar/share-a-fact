38b9ef51dec0f94a1bfe302fb306c203bcfb84a4
Riley Sklar <riley.sklar@gmail.com>
2023-02-02 14:58:23 -0600

    margin bottom h1

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const form = document.querySelector(".fact-form");
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
-    btn.textContent = "Close";
+    btn.textContent = "close";
   } else {
     form.classList.add("hidden");
     btn.textContent = "share-a-fact";
