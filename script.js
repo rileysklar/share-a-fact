ef3d34cae52949458365197c591186a44e997e57
Riley Sklar <riley.sklar@gmail.com>
2023-02-02 14:54:57 -0600

    still lost here

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello world!");
+// console.log("hello world!");
 
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
@@ -13,6 +13,22 @@ btn.addEventListener("click", function () {
   }
 });
 
-let votesInteresting = 23;
-let votesMindblowing = 5;
-const text = "Lisbon is the capital of Protugal";
+// let votesInteresting = 23;
+// let votesMindblowing = 5;
+// const text = "Lisbon is the capital of Protugal";
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+
+const age1 = calcFactAge(20);
+console.log(age1);
+
+let votesInteresting = 20;
+let votesMindblowing = 20;
+
+if (votesInteresting === votesMindblowing) {
+  alert("This fact is equally interesting and mindblowing");
+}
