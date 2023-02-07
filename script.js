f9b844c6f2bc1d5c9789290da01e8a1bff369441
Riley Sklar <riley.sklar@gmail.com>
2023-02-07 16:14:45 -0600

    update on tuesday

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
 // console.log("hello world!");
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting Dom Elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
+// Create DOM Elements: Render Facts in list
+factsList.innerHTML = "";
+createFactsList(initialFacts);
+
+function createFactsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
+  </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -12,23 +83,3 @@ btn.addEventListener("click", function () {
     btn.textContent = "share-a-fact";
   }
 });
-
-// let votesInteresting = 23;
-// let votesMindblowing = 5;
-// const text = "Lisbon is the capital of Protugal";
-
-function calcFactAge(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
-  return age;
-}
-
-const age1 = calcFactAge(20);
-console.log(age1);
-
-let votesInteresting = 20;
-let votesMindblowing = 20;
-
-if (votesInteresting === votesMindblowing) {
-  alert("This fact is equally interesting and mindblowing");
-}
